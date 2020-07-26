c742ab1236c176f2a616b0687a5d82eb7282fddf
sunil-desktop <sunilparajuli2002@gmail.com>
2020-07-27 00:11:05 +0545

    enabled sparrow sms

--- src/account/views.py
+++ src/account/views.py
@@ -179,18 +179,18 @@ class ValidatePhoneSendOTP(APIView):
 							mobile = mobile,
 							otp = key,
 							)
-					# r = requests.post(
-					# "http://api.sparrowsms.com/v2/sms/",
-					# data={'token' : settings.SPARROW_SMS_TOKEN,
-					# 'from'  : settings.SMS_FROM,
-					# 'to'    : mobile,
-					# 'text'  : 'your mobile verification code is  ' + str(key)})
+					r = requests.post(
+					"http://api.sparrowsms.com/v2/sms/",
+					data={'token' : settings.SPARROW_SMS_TOKEN,
+					'from'  : settings.SMS_FROM,
+					'to'    : mobile,
+					'text'  : 'your mobile verification code is  ' + str(key)})
 
-					# status_code = r.status_code
-					# response = r.text
-					# response_json = r.json()
-					# print(status_code)
-					# print(response_json)
+					status_code = r.status_code
+					response = r.text
+					response_json = r.json()
+					print(status_code)
+					print(response_json)
 					print(key)
 					# send_mail(
 					# 'Thank you for your registration',
