3214ea6a0054fdf4b29bacabff8b93410f538dea
abc <sunilparajuli@mac.com>
2021-02-01 22:15:34 +0545

    changes

--- src/account/views.py
+++ src/account/views.py
@@ -906,9 +906,9 @@ from django.db.models import Q
 class MissCallUsersAPIView(APIView):
 	def get(self, request):
 		users = User.objects.all()
-		un_matched_users = CallLog.objects.filter(is_existing=False)#.filter(~Q(number__in=users.values_list('mobile')))
+		un_matched_users = CallLog.objects#.filter(is_existing=False)#.filter(~Q(number__in=users.values_list('mobile')))
 		print(un_matched_users)
-		data = CallLogSerializer(un_matched_users , many=True).data
+		data = CallLogSerializer(un_matched_users.all().order_by('-created_at') , many=True).data
 	
 		return Response(data)
 
